2544da10318b057d1bbb38c9d232e8388b5fc7c2
SANTE\gumougeo <guillaume.mougeot@laposte.net>
2023-11-01 15:21:21 +0100

    clean v1

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-apt-get update
-apt-get install -y nginx jq
+# apt-get update
+# apt-get install -y nginx jq
 
 cd omero-docker
 docker compose up -d
+docker exec -it omeroserver chmod +x /opt/omero/init/populate.sh
 docker exec -it omeroserver /opt/omero/init/populate.sh
+
+sed -i '/^PasswordAuthentication/s/no/yes/' /etc/ssh/sshd_config
+useradd -m -p omero -s /bin/bash user
